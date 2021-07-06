c2782c2ee15239e27536d95039652cd4be3ad04f
Alex Norton <atn362@gmail.com>
2021-07-06 12:13:05 -0500

    home page updates

--- src/components/home.js
+++ src/components/home.js
@@ -11,6 +11,30 @@ class Home extends Component {
                         src="https://atn362.github.io/updated-portfolio/images/IMG_5433.jpeg"
                         alt="profile-pic"
                         className="profile-pic" />
+
+                        <div className="banner-text">
+                            <h1>Full Stack Junior Developer</h1>
+
+                        <hr/>
+                        <p>HTML/CSS | JavaScript | React | NodeJS | Express | MongoDB</p>
+
+                        <div className="social-links">
+                            {/*LinkedIn Icon Link*/}
+                            <a href="https://www.linkedin.com/in/alex-norton-530aa2a8/" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square"  aria-hidden="true" />
+                            </a>
+
+                             {/*GitHub Icon Link*/}
+                             <a href="https://github.com/atn362" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square"  aria-hidden="true" />
+                            </a>
+
+                             {/*Twitter Icon Link*/}
+                             <a href="https://twitter.com/AlexNor77911064" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-twitter-square"  aria-hidden="true" />
+                            </a>
+                        </div>
+                        </div>
                     </Cell>
                 </Grid>
             </div>
