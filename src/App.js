2e7c0e8135fd6088438f4f5a6bb07711921c1210
Alex Norton <atn362@gmail.com>
2021-07-06 11:46:23 -0500

    updated homepage

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header title="Portfolio" scroll>
+        <Header className="header-color" title=" Alex Norton's Portfolio" scroll>
             <Navigation>
+                <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/resume">Resume</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Portfolio">
+        <Drawer title="Alex Norton's Portfolio">
             <Navigation>
+                <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/resume">Resume</Link>
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl'
 
 class Home extends Component {
     render() {
         return(
-            <div><h1>Home Page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="home-grid">
+                    <Cell col={12}>
+                        <img
+                        src="https://atn362.github.io/updated-portfolio/images/IMG_5433.jpeg"
+                        alt="profile-pic"
+                        className="profile-pic" />
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
