12124c7c8468b893087e1c8d6674befe08e01519
Alex Norton <atn362@gmail.com>
2021-07-06 11:20:42 -0500

    updated components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import { Layout, Header, Drawer, Navigation, Content } from "react-mdl";
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-   <div>
-     <h1>Hello World</h1>
-   </div>
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Portfolio" scroll>
+            <Navigation>
+                <Link to="/about">About</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Portfolio">
+            <Navigation>
+                <Link to="/about">About</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main></Main>
+        </Content>
+    </Layout>
+</div>  
 
-  );
+);
 }
 
 export default App;
---
+++ src/components/about.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return(
+            <div><h1>About Page</h1>
+            <header>
+                Welcome to my page.  My name is Alex Norton.  A student on my way to becoming a fullstack, junior developer.
+            </header>
+            
+            
+            
+            </div>
+
+
+
+        )
+    }
+}
+
+export default About;
