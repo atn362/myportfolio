ed853f39f1efe0b9190e4c82eb5b677306539485
Alex Norton <atn362@gmail.com>
2021-07-07 17:19:31 -0500

    contact information and icons added to component

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,20 +19,41 @@ class Contact extends Component {
           <Cell col={6}>
             <h2>Contact Me</h2>
             <hr />
+            <div className="contact-list">
             <List>
               <ListItem>
-                <ListItemContent icon="person"><a href="mailto:atn362@gmail.com">Contact Through Email</a></ListItemContent>
+                <ListItemContent>
+                  <i className="fa fa-envelope" aria-hidden="true" />
+                  <a href="mailto:atn362@gmail.com">  Contact Through Email</a></ListItemContent>
               </ListItem>
               <ListItem>
-                <ListItemContent icon="person"><a href="tel:952-270-8875">Contact Through Telephone</a></ListItemContent>
+                <ListItemContent>
+                  <i className="fa fa-phone-square" aria-hidden="true" />
+                  <a href="tel:952-270-8875">  Contact Through Telephone</a></ListItemContent>
               </ListItem>
               <ListItem>
-                <ListItemContent icon="person"></ListItemContent>
+                <ListItemContent>
+                <i className="fa fa-github-square" aria-hidden="true" />
+                <a href="https://github.com/atn362"   rel="noopener noreferrer"
+                  target="_blank">  Contact Through GitHub</a>
+                </ListItemContent>
               </ListItem>
               <ListItem>
-                <ListItemContent icon="person"></ListItemContent>
+                <ListItemContent>
+                <i className="fa fa-linkedin-square" aria-hidden="true" />
+                <a href="https://www.linkedin.com/in/alex-norton-530aa2a8/"   rel="noopener noreferrer"
+                  target="_blank"> Contact Through LinkedIn</a>
+                </ListItemContent>
+              </ListItem>
+              <ListItem>
+                <ListItemContent>
+                <i className="fa fa-twitter-square" aria-hidden="true" />
+                <a href="https://twitter.com/AlexNor77911064"   rel="noopener noreferrer"
+                  target="_blank"> Contact Through Twitter</a>
+                </ListItemContent>
               </ListItem>
             </List>
+            </div>f
           </Cell>
         </Grid>
       </div>
