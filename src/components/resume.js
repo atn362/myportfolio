55e2dd97934a799a27bc6940a867ef951c85a0d6
Alex Norton <atn362@gmail.com>
2021-07-07 15:43:39 -0500

    resume resized and placed side by side

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { CardTitle, Card } from 'react-mdl';
+import { CardTitle, Card, Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
     render() {
@@ -13,13 +13,16 @@ class Resume extends Component {
             </div>
             
             
-            <Card shadow={0} style={{width: '350px', height: '350px', background: 'url(https://user-images.githubusercontent.com/77468756/124702622-43edbc80-deb6-11eb-86e7-ee816ac121a6.png) center / cover', margin: 'auto'}}>
+            <div className="resume-body">
+            <Grid className="resume-grid">
+                <Cell col={6}>       <Card shadow={0} style={{width: '350px', height: '350px', background: 'url(https://user-images.githubusercontent.com/77468756/124702622-43edbc80-deb6-11eb-86e7-ee816ac121a6.png) center / cover', margin: 'auto'}}>
     <CardTitle expand />
-</Card>
-
-<Card shadow={0} style={{width: '350px', height: '350px', background: 'url(https://user-images.githubusercontent.com/77468756/124702626-45b78000-deb6-11eb-97a7-d9a072246fa9.png) center / cover', margin: 'auto'}}>
+</Card></Cell>
+                <Cell col={6}><Card shadow={0} style={{width: '350px', height: '350px', background: 'url(https://user-images.githubusercontent.com/77468756/124702626-45b78000-deb6-11eb-97a7-d9a072246fa9.png) center / cover', margin: 'auto'}}>
 <CardTitle expand />
-</Card>
+</Card></Cell>
+            </Grid>
+            </div>
 
 
 </div>
