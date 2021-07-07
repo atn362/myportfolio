6ad4e402d8ff183fd2c3e3c0c6484cd4f26cdfc4
Alex Norton <atn362@gmail.com>
2021-07-07 16:03:39 -0500

    contact pic and message added

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,12 @@ class Contact extends Component {
         return(
             <div className="contact-body">
             <Grid className="contact-grid">
-                <Cell col={6}></Cell>
+                <Cell col={6}>
+                    <h2>Alexander Norton</h2>
+                    <img
+                    src="https://atn362.github.io/updated-portfolio/images/IMG_5433.jpeg" alt="profile" style={{height: '150px'}} b />
+                    <h3>Please contact me through any of the following means.</h3>
+                </Cell>
                 <Cell col={6}></Cell>
             </Grid>
             </div>
